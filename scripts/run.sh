5a94e63d73355825db6e1be8befbc3253a1b38f6
ExecuteAutomation <kartmcad@gmail.com>
2022-04-11 11:44:13 +1200

    added sh file

---
+++ scripts/run.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# Run the dotnet app
+dotnet run --project ./GraphQL.NET.sln
+
+#install newman
+npm install -g newman
+#Run newman collection and environment
+newman run ./postman/collection.json -e ./postman/environment.json --reporters cli --insecure
+
+if [ $? -eq 0 ] 
+then 
+  echo "Successfully executed newman test" 
+  exit 0 
+else 
+  echo "Failing executing newman test" >&2 
+  exit 1 
+fi
+
