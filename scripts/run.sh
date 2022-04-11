f78e161a9cea6670f5625e756d13f652ac92c9cb
Karthik KK <karthik@techgeek.co.in>
2022-04-11 16:01:53 +1200

    fixed compose file

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,9 +10,9 @@ export COMPOSE_HTTP_TIMEOUT=200
 docker-compose build
 
 docker-compose up -d graphql_api
-docker-compose up --no-deps newman_test
+docker-compose up --no-deps newman
 
-exit_code=$(docker inspect newman_test -f '{{ .State.ExitCode }}')
+exit_code=$(docker inspect newmantest -f '{{ .State.ExitCode }}')
 
 if [ $exit_code -eq 0 ]; then
     exit $exit_code
