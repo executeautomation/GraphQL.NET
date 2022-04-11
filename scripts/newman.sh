5c60124ed4ceba4a8d4c6cdfceccc6a7ef83e466
Karthik KK <karthik@techgeek.co.in>
2022-04-11 15:34:32 +1200

    added run file

--- scripts/newman.sh
+++ scripts/newman.sh
@@ -16,3 +16,5 @@ else
   echo "Failing executing newman test" >&2 
   exit
 fi
+
+
---
+++ scripts/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+set -e
+set -x
+
+cd "$(dirname "${0}")/.."
+
+export COMPOSE_HTTP_TIMEOUT=200
+
+docker-compose build
+
+docker-compose up -d graphqlapi
+docker-compose up --no-deps nodejs
+
+exit_code=$(docker inspect nodejs -f '{{ .State.ExitCode }}')
+
+if [ $exit_code -eq 0 ]; then
+    exit $exit_code
+else
+    echo "Test failed"
+fi
+
